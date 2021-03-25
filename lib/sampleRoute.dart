d30851d7fc2eed8dbf8dc8e3b39dcd713b7abe54
gopalbbharadva <gopalbbharadva@gmail.com>
2021-03-25 22:48:54 +0530

    day 6 completed

--- lib/sampleRoute.dart
+++ lib/sampleRoute.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:newstart/utils/routes.dart';
 
-class sampleRoute extends StatelessWidget {
+class sampleRoute extends StatefulWidget {
+  @override
+  _sampleRouteState createState() => _sampleRouteState();
+}
+
+class _sampleRouteState extends State<sampleRoute> {
+  String name = "";
+
+  bool flag = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,59 +19,84 @@ class sampleRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Sign in"),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "Asset/images/sign_in.png",
-                height: 300,
-                fit: BoxFit.fitWidth,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Welcome to Signin",
-            style: GoogleFonts.lato(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextField(
-                  decoration: InputDecoration(
-                      hintText: "Enter username:", labelText: "Username"),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      hintText: "Enter password:", labelText: "Password"),
+                Image.asset(
+                  "Asset/images/sign_in.png",
+                  height: 300,
+                  fit: BoxFit.fitWidth,
                 ),
               ],
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            style: TextButton.styleFrom(
-              shadowColor: Colors.amberAccent,
-              elevation: 5.0,
-              backgroundColor: Colors.blueGrey,
-              minimumSize: Size(120, 40),
+            SizedBox(
+              height: 10,
             ),
-            onPressed: () {
-              Navigator.pushNamed(context, routes.homepage);
-            },
-            child: Text("Sign in"),
-          )
-        ],
+            Text(
+              "Welcome " + name,
+              style:
+                  GoogleFonts.lato(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: Column(
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Enter username:", labelText: "Username"),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter password:", labelText: "Password"),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () async {
+                setState(
+                  () {
+                    flag = true;
+                  },
+                );
+                await Future.delayed(Duration(seconds: 1));
+                Navigator.pushNamed(context, routes.homepage);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 800),
+                alignment: Alignment.center,
+                width: flag ? 60 : 130,
+                height: 40,
+                child: flag
+                    ? Icon(
+                        Icons.login,
+                        color: Colors.white,
+                      )
+                    : Text(
+                        "Sign In",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(flag ? 50 : 8),
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
