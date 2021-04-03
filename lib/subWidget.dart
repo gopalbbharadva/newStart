4795b45ae3975ffc98ec71c4e7dfd0d62f83d5e9
gopalbbharadva <gopalbbharadva@gmail.com>
2021-04-03 21:52:55 +0530

    day 13 completed(jsonmapstart)

--- lib/subWidget.dart
+++ lib/subWidget.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:newstart/ItemWidget.dart';
 import 'package:newstart/models/items.dart';
 import 'package:newstart/sideDrawer.dart';
 
-class subWidget extends StatelessWidget {
+class subWidget extends StatefulWidget {
+  @override
+  _subWidgetState createState() => _subWidgetState();
+}
+
+class _subWidgetState extends State<subWidget> {
   int day = 20;
+
   String place = "Codepur";
 
   final dummyList = List.generate(12, (index) => Product.items[0]);
+
+  @override
+  void initState() {
+    super.initState();
+
+    jsonData();
+  }
+
+  void jsonData() async {
+    var catalogData = await rootBundle.loadString("Asset/Data/catalog.json");
+    var jsonData = jsonDecode(catalogData);
+    var products = jsonData['products'];
+    print(products);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
